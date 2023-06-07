12f73499ce571518bc69ec2e68e619ea14cdab7b
Vaibhav Shukla <mickeyvaibhav@gmail.com>
2023-06-08 01:45:17 +0530

    Update autoredirect.py

--- autoredirect.py
+++ autoredirect.py
@@ -18,7 +18,7 @@ parameter_names = [
     'action', 'action_url', 'Redirect', 'sp_url', 'service', 'recurl',
     'j?url', 'url=//', 'uri', 'u', 'allinurl:', 'q', 'link', 'src', 'tc?src',
     'linkAddress', 'location', 'burl', 'request', 'backurl', 'RedirectUrl',
-    'Redirect', 'ReturnUrl', 'redirecturl'
+    'Redirect', 'ReturnUrl', 'redirecturl', 'checkout_url', 'return_url', 'authorize_callback'
 ]
 
 # Read URLs from file
@@ -32,41 +32,66 @@ print('Check URLs for keywords and test for open redirection')
 
 def check_url(url):
     found_keyword = False
-    redirect_url = 'https://www.google.com'
-    # Check if URL contains any keyword
-    for keyword in parameter_names:
-        pattern = r'([?&])(' + re.escape(keyword) + r'=)'
-        match = re.search(pattern, url)
 
-        if match:
-            found_keyword = True
-            url = re.sub(r'(\?|&)' + re.escape(keyword) + '=([^&]*)', r'\1' + keyword + '=' + redirect_url, url)
-            break
+# also check for https%3A%2F%2Fwww.google.com%2F https://www%2Egoogle%2Ecom  
+# https://www%252Egoogle%252Ecom  
+# http://%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D   
+# ///www.google.com/  
+#   //www.google.com
+#   ////www.google.com
 
-    if found_keyword:
-        # Test for open redirection by redirecting to www.google.com
-        try:
-            response = requests.get(url, allow_redirects=False, verify=False)
-        except requests.exceptions.RequestException as e:
-            print('\033[91m[Error]\033[0m', url, '--', str(e))
-            return url, False
 
-        # Analyze the response to determine vulnerability
-        if 300 <= response.status_code <= 309 and redirect_url in response.headers.get('Location', ''):
-            print('\033[91m[Vulnerable]\033[0m', url)  # Print in red for vulnerable ones
-            return url, True
-        elif response.status_code == 301 and redirect_url in response.headers.get('Location', ''):
-            print('\033[91m[Vulnerable]\033[0m', url)  # Print in red for vulnerable ones
-            return url, True
+    variations = [
+            '///www.google.com/',
+            '//www.google.com'
+            '////www.google.com',
+            'https://www.google.com',
+            'https%3A%2F%2Fwww.google.com%2F',
+            'https://www%2Egoogle%2Ecom',
+            'https%3A%2F%2Fwww%252Egoogle%252Ecom',
+            'http://%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D'
+        ]
+
+    is_vulnerable = False  # Flag variable to track vulnerability
+
+    for variation in variations:    
+        # Check if URL contains any keyword
+        for keyword in parameter_names:
+            pattern = r'([?&])(' + re.escape(keyword) + r'=)'
+            match = re.search(pattern, url)
+
+            if match:
+                found_keyword = True
+                url = re.sub(r'(\?|&)' + re.escape(keyword) + '=([^&]*)', r'\1' + keyword + '=' + variation, url)
+                break
+
+        if found_keyword:
+            # Test for open redirection by redirecting to www.google.com
+            try:
+                response = requests.get(url, allow_redirects=False, verify=False)
+            except requests.exceptions.RequestException as e:
+                print('\033[91m[Error]\033[0m', url, '--', str(e))
+                continue  # Continue to the next variation
+
+            # Analyze the response to determine vulnerability
+            if 300 <= response.status_code <= 309 and variation in response.headers.get('Location', ''):
+                print('\033[91m[Vulnerable]\033[0m', url)  # Print in red for vulnerable ones
+                is_vulnerable = True
+            else:
+                print('\033[92m[Non-Vulnerable]\033[0m', url)  # Print in green for non-vulnerable ones
         else:
-            print('\033[92m[Non-Vulnerable]\033[0m', url)  # Print in green for non-vulnerable ones
-            return url, False
-    else:
-        print('\033[97m[Ignored]\033[0m', url)  # Print in white for ignored ones
-        return url, False
+            print('\033[97m[Ignored]\033[0m', url)  # Print in white for ignored ones
+
+        # Return the result if a vulnerable URL is found
+        if is_vulnerable:
+            return url, True
+
+    # Return the result after the loop ends
+    return url, False
+
 
 # Use ThreadPoolExecutor to run the loops in parallel
-with concurrent.futures.ThreadPoolExecutor() as executor:
+with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
     results = executor.map(check_url, urls)
 
 # Collect the results
