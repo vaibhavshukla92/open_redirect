50a8cfc5e891cd35388f018f980d854217fdbe36
Vaibhav Shukla <mickeyvaibhav@gmail.com>
2023-06-10 22:31:38 +0530

    Update autoredirect.py

--- autoredirect.py
+++ autoredirect.py
@@ -33,28 +33,20 @@ print('Check URLs for keywords and test for open redirection')
 def check_url(url):
     found_keyword = False
 
-# also check for https%3A%2F%2Fwww.google.com%2F https://www%2Egoogle%2Ecom  
-# https://www%252Egoogle%252Ecom  
-# http://%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D   
-# ///www.google.com/  
-#   //www.google.com
-#   ////www.google.com
-
-
     variations = [
-            '///www.google.com/',
-            '//www.google.com'
-            '////www.google.com',
-            'https://www.google.com',
-            'https%3A%2F%2Fwww.google.com%2F',
-            'https://www%2Egoogle%2Ecom',
-            'https%3A%2F%2Fwww%252Egoogle%252Ecom',
-            'http://%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D'
-        ]
+        '///www.google.com/',
+        '//www.google.com',
+        '////www.google.com',
+        'https://www.google.com',
+        'https%3A%2F%2Fwww.google.com%2F',
+        'https://www%2Egoogle%2Ecom',
+        'https%3A%2F%2Fwww%252Egoogle%252Ecom',
+        'http://%77%77%77%2E%67%6F%6F%67%6C%65%2E%63%6F%6D'
+    ]
 
     is_vulnerable = False  # Flag variable to track vulnerability
 
-    for variation in variations:    
+    for variation in variations:
         # Check if URL contains any keyword
         for keyword in parameter_names:
             pattern = r'([?&])(' + re.escape(keyword) + r'=)'
@@ -62,43 +54,48 @@ def check_url(url):
 
             if match:
                 found_keyword = True
-                url = re.sub(r'(\?|&)' + re.escape(keyword) + '=([^&]*)', r'\1' + keyword + '=' + variation, url)
+                url_new = re.sub(r'(\?|&)' + re.escape(keyword) + '=([^&]*)', r'\1' + keyword + '=' + variation, url)
                 break
 
         if found_keyword:
             # Test for open redirection by redirecting to www.google.com
             try:
-                response = requests.get(url, allow_redirects=False, verify=False)
+                response = requests.get(url_new, allow_redirects=False, verify=False)
             except requests.exceptions.RequestException as e:
-                print('\033[91m[Error]\033[0m', url, '--', str(e))
+                print('\033[91m[Error]\033[0m', url_new, '--', str(e))
                 continue  # Continue to the next variation
 
             # Analyze the response to determine vulnerability
-            if 300 <= response.status_code <= 309 and variation in response.headers.get('Location', ''):
-                print('\033[91m[Vulnerable]\033[0m', url)  # Print in red for vulnerable ones
+            location = response.headers.get('Location', '')
+
+            if 300 <= response.status_code <= 309 and (location.startswith('https://www.google.com') or location.startswith(variation)):
+                print('\033[91m[Vulnerable]\033[0m', url_new)  # Print in red for vulnerable ones
                 is_vulnerable = True
+                print(location)
+                vulnerable_urls.append(url_new)
+                break
             else:
-                print('\033[92m[Non-Vulnerable]\033[0m', url)  # Print in green for non-vulnerable ones
-        else:
-            print('\033[97m[Ignored]\033[0m', url)  # Print in white for ignored ones
+                print('\033[92m[Non-Vulnerable]\033[0m', url_new)  # Print in green for non-vulnerable ones
+                print(location)
+                break
 
-        # Return the result if a vulnerable URL is found
-        if is_vulnerable:
-            return url, True
+    if not found_keyword:
+        print('\033[97m[Ignored]\033[0m', url)  # Print in white for ignored ones
 
-    # Return the result after the loop ends
-    return url, False
-
+    return url, is_vulnerable
 
 # Use ThreadPoolExecutor to run the loops in parallel
 with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
     results = executor.map(check_url, urls)
 
 # Collect the results
+processed_urls = set()
 for result in results:
     url, is_vulnerable = result
-    if is_vulnerable:
-        vulnerable_urls.append(url)
+    processed_urls.add(url)
+
+# Remove processed URLs from the original list
+urls = list(set(urls) - processed_urls)
 
 print('Write vulnerable URLs to a file')
 with open('vulnerable.txt', 'w') as file:
